a6a7e3a47629274b33104d8e053e5ca185e13ab1
dush-ik <dushik@live.com>
2022-06-12 20:28:36 +0530

    glowing added

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
-(() => {
-  const player1 = prompt('Enter player 1 name');
-  const player2 = prompt('Enter player 2 name');
-  const $playerNameList = document.getElementsByClassName('player-name');
-  $playerNameList[0].textContent = player1;
-  $playerNameList[1].textContent = player2;
-})();
+const $playerScore = document.getElementById('player-score');
+const $playerPits = document.getElementById('player-pits');
+const $computerScore = document.getElementById('computer-score');
+const $computerPits = document.getElementById('computer-pits');
+
+const start = () => {
+  $playerScore.textContent = '0';
+  $computerScore.textContent = '0';
+  Array.from($playerPits.children).forEach($ele => $ele.textContent = '6');
+  Array.from($computerPits.children).forEach($ele => $ele.textContent = '6');
+}
+
+const chooseTurn = () => {
+
+}
+
+start();
